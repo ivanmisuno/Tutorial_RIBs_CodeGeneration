61369e2674193cc6761af4d41b7798453c6c558c
Ivan Misuno <im@popappfactory.com>
2018-10-31 23:41:17 +0100

    Initial commit - an empty single-view app from the Xcode template.
    Summary:
    An empty single-view app from the Xcode template.

---
+++ Tutorial_RIBs_CodeGeneration/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Tutorial_RIBs_CodeGeneration
+//
+//  Created by Ivan Misuno on 31-10-2018.
+//  Copyright © 2018 ivanmisuno. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ Tutorial_RIBs_CodeGeneration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tutorial_RIBs_CodeGeneration
+//
+//  Created by Ivan Misuno on 31-10-2018.
+//  Copyright © 2018 ivanmisuno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
